598a306e6f67eb7fcee11ba7855d7605650ed2ba
Alexandre Fenyo <fenyo@mac.fenyo.net>
2018-04-17 00:01:24 +0200

    updates

--- iOS tools/AppDelegate.swift
+++ iOS tools/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        GenericTools.here("application()", self)
+
         return true
     }
 
---
+++ iOS tools/Tools/GenericTools.swift
@@ -0,0 +1,44 @@
+//
+//  GenericTools.swift
+//  iOS tools
+//
+//  Created by Alexandre Fenyo on 16/04/2018.
+//  Copyright © 2018 Alexandre Fenyo. All rights reserved.
+//
+
+// "alt" means "alternative"
+
+import Foundation
+
+class GenericTools {
+
+    // Basic debugging
+    // Can be declared these ways:
+    // static func here() -> () {
+    // static func here() -> Void {
+    // static func here() {
+    static func here() {
+        print("here");
+    }
+
+    // Basic debugging
+    // ex.: here("here")
+    //      here("here", self)
+    static func here(_ s: String, _ o: Any? = nil) {
+        if o == nil {
+            print("here:", s);
+        } else {
+            // print(o.debugDescription) prints the instance as an Optional
+            // print(o!.debugDescription) does not compile ("Value of type 'Any' has no member 'debugDescription'")
+            print("here: ", o!, ":", s, separator: String() /* alt: separator: "" */)
+        }
+    }
+    
+    // The previous function can also we written with o as an implicitely unwrapped Optional wrapping an instance of Any?, like this:
+    // static func here(_ s: String, _ o: Any?! = nil) {
+    // ...
+    // print("here:", s, "instance:", o)
+    // ...
+    
+
+}
