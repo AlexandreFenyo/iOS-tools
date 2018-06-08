9c9bff1c3f14670ed61834d1a16fdb2566eb3f34
Alexandre Fenyo <fenyo@mac-de-alexandre.local>
2018-06-08 19:46:57 +0200

    update

--- iOS tools/Tools/NetTools.swift
+++ iOS tools/Tools/NetTools.swift
@@ -8,56 +8,54 @@
 
 import Foundation
 
+// bug actuel :
+// nc localhost 1919  < /dev/urandom
 
-// https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/NetServices/Introduction.html
 // https://github.com/ecnepsnai/BonjourSwift/blob/master/Bonjour.swift
-// https://github.com/Bouke/NetService
+// https://docs.swift.org/swift-book/LanguageGuide/AutomaticReferenceCounting.html
 // nc localhost 1919
 // dig -p 5353 @mac _ssh._tcp.local. ptr
 // dig -p 5353 @224.0.0.251 _chargen._tcp.local. ptr
+// https://medium.com/flawless-app-stories/memory-leaks-in-swift-bfd5f95f3a74
 
+// Default values
+struct NetworkDefaults {
+    static let chargen_port : Int32 = 1919
+}
 
-class MyThread : Thread {
-    public var r : RunLoop?
-    
-    @objc
-    func saveR(_ gesture: Int) {
-        print("saveR:", OperationQueue.current)
-        r = RunLoop.current
-    }
-    
-    public override func main() {
-        print("MYTHREAD")
+class BackgroundNetworkThread : Thread {
+    public var run_loop : RunLoop?
 
-        // exécuter dans le mainthread l'affectation de la source
+    override public func main() {
+        // Create the run loop
+        run_loop = RunLoop.current
 
-        if r == nil { saveR(5) }
-        print("main:", OperationQueue.current)
-        r!.run()
-        print("FIN MYTHREAD")
+        // Prevent the run loop to exit when there is no registered input source
+        run_loop!.add(Timer(timeInterval: TimeInterval(3600), repeats: true, block: { _ in }), forMode: RunLoopMode.commonModes)
+
+        // Handle events in the background thread
+        run_loop!.run()
     }
 }
 
 class MyNetServiceBrowserDelegate : NSObject, NetServiceBrowserDelegate {
+    deinit {
+        print("MyNetServiceBrowserDelegate.deinit")
+    }
+    
     func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
         print("netServiceBrowser:", service, moreComing)
     }
 
-    func netServiceBrowser(_ browser: NetServiceBrowser,
-                           didRemoveDomain domainString: String,
-                           moreComing: Bool) {
+    func netServiceBrowser(_ browser: NetServiceBrowser, didRemoveremoDomain domainString: String, moreComing: Bool) {
         print("didRemoveDomain")
     }
 
-    func netServiceBrowser(_ browser: NetServiceBrowser,
-                           didFindDomain domainString: String,
-                           moreComing: Bool) {
+    func netServiceBrowser(_ browser: NetServiceBrowser, didFindDomain domainString: String, moreComing: Bool) {
         print("didFindDomain")
     }
 
-    func netServiceBrowser(_ browser: NetServiceBrowser,
-                           didRemove service: NetService,
-                           moreComing: Bool) {
+    func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
         print("didRemove")
     }
 
@@ -65,25 +63,43 @@ class MyNetServiceBrowserDelegate : NSObject, NetServiceBrowserDelegate {
         print("netServiceBrowserWillSearch")
     }
 
-    func netServiceBrowser(_ browser: NetServiceBrowser,
-                           didNotSearch errorDict: [String : NSNumber]) {
+    func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
         print("didNotSearch")
-        for err in errorDict {
-            print("ERREUR:", err.key, err.value)
-        }
+        for err in errorDict { print("ERREUR:", err.key, err.value) }
     }
 
     func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
         print("netServiceBrowserDidStopSearch")
     }
-
-
 }
 
-class MyNetServiceDelegate : NSObject, NetServiceDelegate {
-    override init() {
-        super.init()
-        print("init")
+class ChargenClient {
+    public let input_stream : InputStream
+    public let output_stream : OutputStream
+    private let stream_delegate : StreamDelegate
+    
+    public init(input_stream: InputStream, output_stream: OutputStream, stream_delegate: StreamDelegate) {
+        self.input_stream = input_stream
+        self.output_stream = output_stream
+        self.stream_delegate = stream_delegate
+    }
+}
+
+class NetServiceChargenDelegate : NSObject, NetServiceDelegate {
+    private var clients : [ChargenClient] = []
+    
+    deinit {
+        print("NetServiceChargenDelegate.deinit")
+    }
+
+    public func removeClient(stream: Stream) {
+        for i in clients.indices {
+            if (stream == clients[i].input_stream || stream == clients[i].output_stream) {
+                clients.remove(at: i)
+                print("REMOVE")
+                break
+            }
+        }
     }
     
     func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
@@ -117,196 +133,121 @@ class MyNetServiceDelegate : NSObject, NetServiceDelegate {
     func netService(_ sender: NetService, didUpdateTXTRecord data: Data) {
         print("didUpdateTXTRecord")
     }
-    
-    // https://github.com/shogo4405/HaishinKit.swift/blob/master/Sources/Net/NetSocket.swift
-//    var backgroundQueue : DispatchQueue?
-//    func netService(_ sender: NetService, didAcceptConnectionWith inputStream: InputStream, outputStream: OutputStream) {
-//        print("didAcceptConnectionWith")
-//        backgroundQueue = DispatchQueue(label: "net.fenyo.apple.iOS-tools.chargen.read")
-//        backgroundQueue!.async {
-//            inputStream.open()
-//            outputStream.open()
-//
-//            let content = "0123456789"
-//            let data = content.data(using: String.Encoding.utf8, allowLossyConversion: false)!
-//            let dataMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
-//            data.copyBytes(to: dataMutablePointer, count: data.count)
-//            let dataPointer = UnsafePointer<UInt8>(dataMutablePointer)
-//
-//            while true {
-//                print("loop start")
-//
-//                while !outputStream.hasSpaceAvailable {
-//                    print("not hasSpaceAvailable -> stream status:", outputStream.streamStatus.rawValue)
-//                    switch outputStream.streamStatus {
-//                    case .atEnd:
-//                        print("atEnd")
-//                    case .closed:
-//                        print("closed")
-//                    case .error:
-//                        print("error")
-//                    case .notOpen:
-//                        print("notOpen")
-//                    case .open:
-//                        print("open")
-//                        print(Date())
-////                        let ret = outputStream.write(dataPointer, maxLength: data.count)
-////                        print("write->", ret)
-////                        print(Date())
-//                    case .opening:
-//                        print("opening")
-//                    case .reading:
-//                        print("reading")
-//                    case .writing:
-//                        print("writing")
-//                    }
-//                    sleep(1)
-//                }
-//
-//                let ret = outputStream.write(dataPointer, maxLength: data.count)
-//                print("write -> stream status:", outputStream.streamStatus.rawValue)
-//                if ret != data.count { print("write:", ret) }
-//                if ret < 0 {
-//                    print("write error:", outputStream.streamError!)
-//                    break
-//                }
-//                if ret == 0 {
-//                    print("write returned 0:", outputStream.streamError!)
-//                }
-//            }
-//
-//            print("quit")
-//            dataMutablePointer.deallocate()
-//            outputStream.close()
-//            inputStream.close()
-//        }
-    
-    var output_stream_delegate : StreamDelegate?
-    var r : RunLoop?
-    var d : DispatchQueue?
-    var t : MyThread?
 
-    // https://github.com/apple/swift/blob/c760f6dfbf0179e9aff90f7bf7375f3af5331318/docs/proposals/Concurrency.rst
-    // https://medium.com/flawless-app-stories/basics-of-parallel-programming-with-swift-93fee8425287
-    // https://developer.apple.com/library/archive/samplecode/Earthquakes/Listings/Earthquakes_iOS_QuakesViewController_swift.html#//apple_ref/doc/uid/TP40014547-Earthquakes_iOS_QuakesViewController_swift-DontLinkElementID_5
+    // Manage new connections
     func netService(_ sender: NetService, didAcceptConnectionWith inputStream: InputStream, outputStream: OutputStream) {
-        print("didAcceptConnectionWith")
-        output_stream_delegate = MyOutputStreamDelegate()
-        outputStream.delegate = output_stream_delegate
+        let stream_delegate = StreamChargenDelegate()
+        clients.append(ChargenClient(input_stream: inputStream, output_stream: outputStream, stream_delegate: stream_delegate))
 
-        outputStream.open()
-
-        print("didAcceptConnectionWith 2")
-
-        t = MyThread()
-        t!.start()
-
-sleep(3)
-
-        //        t!.perform(#selector(MyThread.saveR(_:)))
-        t!.perform(#selector(MyThread.saveR(_:)), on: t!, with: nil, waitUntilDone: true)
-
-        print("didAcceptConnectionWith 3")
-
-        outputStream.schedule(in: t!.r!, forMode: .commonModes)
-
-        
-//outputStream.schedule(in: <#T##RunLoop#>, forMode: <#T##RunLoopMode#>)
-
-        //        d!.async {
-//            print(CFRunLoopGetCurrent())
-//        }
-
-//        d!.sync {
-//            self.r = .current
-////            print(CFRunLoopGetCurrent())
-////            print(CFRunLoopGetMain())
-//        }
-//
-
-//        print(OperationQueue.current)
-//        print(OperationQueue.main)
-
-        // outputStream.schedule(in: r!, forMode: .commonModes)
-
-    
-    }
-}
-
-class MyOutputStreamDelegate : NSObject, StreamDelegate {
-    let content : String
-    let data : Data
-    let dataMutablePointer : UnsafeMutablePointer<UInt8>
-    let dataPointer : UnsafePointer<UInt8>
-
-    override init() {
-        content = "0123456789"
-        data = content.data(using: String.Encoding.utf8, allowLossyConversion: false)!
-        dataMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
-        data.copyBytes(to: dataMutablePointer, count: data.count)
-        dataPointer = UnsafePointer<UInt8>(dataMutablePointer)
-    }
-    
-    public func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
-        switch eventCode {
-        case .openCompleted:
-            print("openCompleted")
-        case .hasBytesAvailable:
-            print("hasBytesAvailable")
-        case .hasSpaceAvailable:
-            print("hasSpaceAvailable")
-            let outputStream = aStream as! OutputStream
-            let ret = outputStream.write(dataPointer, maxLength: data.count)
-print(OperationQueue.current)
-//            while outputStream.hasSpaceAvailable {
-//                outputStream.write(dataPointer, maxLength: data.count)
-//            }
-
-            //            print(DispatchQueue.main)
-//            print(OperationQueue.current)
-//            print(OperationQueue.main)
-//            print(CFRunLoopGetCurrent())
-//            print(CFRunLoopGetMain())
-
-            //            while true { sleep(1000) }
-            print("write->", ret)
-
-        case .errorOccurred:
-            print("errorOccurred")
-        case .endEncountered:
-            print("endEncountered")
-        default:
-            print("default")
+        // Handle stream callbacks on a background thread
+        for s in [inputStream, outputStream] {
+            s.delegate = stream_delegate
+            s.open()
+            s.schedule(in: NetTools.background_network_thread!.run_loop!, forMode: .commonModes)
         }
     }
 }
 
+class StreamChargenDelegate : NSObject, StreamDelegate {
+    private let content = "0123456789"
+    private let data : Data
+    private let dataMutablePointer : UnsafeMutablePointer<UInt8>
+    private let dataPointer : UnsafePointer<UInt8>
 
+    private let bufMutablePointer : UnsafeMutablePointer<UInt8>
+    private let bufPointer : UnsafePointer<UInt8>
+
+    deinit {
+        print("StreamChargenDelegate.deinit")
+    }
+
+    override init() {
+        data = content.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+        dataMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
+        data.copyBytes(to: dataMutablePointer, count: data.count)
+        dataPointer = UnsafePointer<UInt8>(dataMutablePointer)
+
+        bufMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: 10)
+        bufPointer = UnsafePointer<UInt8>(bufMutablePointer)
+    }
+    
+    public func stream(_ stream: Stream, handle event_code: Stream.Event) {
+        switch event_code {
+        case .openCompleted:
+            print("openCompleted")
+
+        case .hasBytesAvailable:
+//            print("hasBytesAvailable")
+            let inputStream = stream as! InputStream
+            let ret = inputStream.read(bufMutablePointer, maxLength: 10)
+//            print("read=->", ret)
+
+        case .hasSpaceAvailable:
+//            print("hasSpaceAvailable")
+            let outputStream = stream as! OutputStream
+            let ret = outputStream.write(dataPointer, maxLength: data.count)
+  //          print("write->", ret)
+
+        case .errorOccurred:
+            print("errorOccurred")
+            end(stream)
+            
+        case .endEncountered:
+            print("endEncountered")
+
+        default:
+            print("default")
+        }
+    }
+    
+    private func end(_ stream: Stream) {
+        DispatchQueue.main.sync {
+            // Implicitely remove the stream from the run loop
+//            stream.close()
+            
+            // May be useless since the delegate property is declared unowned(unsafe)
+            stream.delegate = nil
+            
+//            NetTools.net_service_chargen_delegate!.removeClient(stream: stream)
+
+         stream.close()
+
+        }
+    }
+    
+}
 
 class NetTools {
-    public static var x = false
-    private static var srv : NetService?
+    public static var background_network_thread : BackgroundNetworkThread?
+
+    private static var net_service_chargen : NetService?
+    public static var net_service_chargen_delegate : NetServiceChargenDelegate?
+
     private static var br : NetServiceBrowser?
-    public static var dl : MyNetServiceDelegate?
     public static var dl2 : MyNetServiceBrowserDelegate?
-    public static var q : DispatchQueue?
-    
-    // https://developer.apple.com/library/archive/documentation/General/Conceptual/ConcurrencyProgrammingGuide/OperationObjects/OperationObjects.html#//apple_ref/doc/uid/TP40008091-CH101-SW1
-    // https://theswiftdev.com/2017/08/29/concurrency-model-in-swift/
-    // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/RunLoopManagement/RunLoopManagement.html
+
+    public static var x = false
+
     public static func initBonjourService() {
         if !x {
             x = true
 
+            // Create and start background thread for networking operations
+            background_network_thread = BackgroundNetworkThread()
+            background_network_thread!.start()
 
+            // Create chargen service
+            net_service_chargen = NetService(domain: "local.", type: "_chargen._tcp.", name: "chargen", port: NetworkDefaults.chargen_port)
 
-            let service = NetService(domain: "local.", type: "_chargen._tcp.", name: "chargen", port: 1919)
-            srv = service
-            dl = MyNetServiceDelegate()
-            service.delegate = dl
-            service.publish(options: .listenForConnections)
-            print("service published")
-        
+            // Add a strong ref to the delegate since NetService.delegate is declared unowned(unsafe)
+            net_service_chargen_delegate = NetServiceChargenDelegate()
+            net_service_chargen!.delegate = net_service_chargen_delegate
+
+            // Start listening for chargen clients
+            net_service_chargen!.publish(options: .listenForConnections)
+
+            
+            
 //            let browser = NetServiceBrowser()
 //            br = browser
 //            dl2 = MyNetServiceBrowserDelegate()
@@ -315,19 +256,10 @@ class NetTools {
 //            browser.searchForServices(ofType: "_chargen._tcp.", inDomain: "local.")
 //            print("browsing")
 
-//            let q = OperationQueue()
-//            var i = 0
-//            q.addOperation {
-//                while true {
-//                    print("operation", i)
-//                    i += 1
-//                }
-//            }
-
 //            // https://developer.apple.com/documentation/corefoundation/1539743-cfreadstreamopen
 //            var readStream : Unmanaged<CFReadStream>?
 //            var writeStream : Unmanaged<CFWriteStream>?
-//            CFStreamCreatePairWithSocketToHost(nil, "localhost" as CFString, 1919, &readStream, &writeStream)
+//            CFStreamCreatePairWithSocketToHost(nil, "localhost" as CFString, NetworkDefaults.chargen_port, &readStream, &writeStream)
 //            CFReadStreamOpen(readStream!.takeRetainedValue())
         }
 
