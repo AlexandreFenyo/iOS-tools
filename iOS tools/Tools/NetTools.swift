fffdd0ff4ef585c0a7792b2aa774245a7ab335c6
Alexandre Fenyo <fenyo@mac.fenyo.net>
2018-06-12 01:43:11 +0200

    update

--- iOS tools/Tools/NetTools.swift
+++ iOS tools/Tools/NetTools.swift
@@ -8,9 +8,8 @@
 
 import Foundation
 
-// bug actuel :
-// nc localhost 1919  < /dev/urandom
-
+// (while sleep .1; do echo salut ; done) | nc 192.168.1.212 1919 > /tmp/bigfile
+// simuler one-way: CTRL-D avec nc sur MacOS
 // https://github.com/ecnepsnai/BonjourSwift/blob/master/Bonjour.swift
 // https://docs.swift.org/swift-book/LanguageGuide/AutomaticReferenceCounting.html
 // nc localhost 1919
@@ -20,7 +19,8 @@ import Foundation
 
 // Default values
 struct NetworkDefaults {
-    static let chargen_port : Int32 = 1919
+    public static let chargen_port : Int32 = 1919
+    public static let buffer_size = 3000
 }
 
 // Protocol used to inform with a callback that a child object has done its job
@@ -28,7 +28,7 @@ protocol RefClosed {
     func refClosed(_: ChargenClient)
 }
 
-// Handle thread and run loop dedicated for a single input or output stream
+// Start a run loop to manage a stream
 class StreamNetworkThread : Thread {
     private let stream : Stream
     
@@ -40,113 +40,136 @@ class StreamNetworkThread : Thread {
     override public func main() {
         stream.open()
         stream.schedule(in: .current, forMode: .commonModes)
-
-        print("START LOOP")
-        RunLoop.current.run(iuntil)
-        print("QUIT LOOP")
-
+        RunLoop.current.run()
         stream.close()
-        // Stream delegate can no more be called for events on this stream
     }
 }
 
+// Manage a remote client with two threads, one for each stream
 class ChargenClient : NSObject, StreamDelegate {
-    public var background_network_thread_in : StreamNetworkThread?
-    public var background_network_thread_out : StreamNetworkThread?
+    private var background_network_thread_in, background_network_thread_out : StreamNetworkThread?
+    private let input_stream, output_stream : Stream
 
     // Needed to inform the the parent that this ChargenClient instance can be disposed
     private weak var from : NetServiceChargenDelegate?
-    
-    private let content = "0123456789"
-    private let data : Data
-    private let dataMutablePointer : UnsafeMutablePointer<UInt8>
-    private let dataPointer : UnsafePointer<UInt8>
-    private let bufMutablePointer : UnsafeMutablePointer<UInt8>
-    private let bufPointer : UnsafePointer<UInt8>
 
-    public func cancelThreads() {
-        background_network_thread_in!.cancel()
-        background_network_thread_out!.cancel()
+    // Data buffers
+    private let dataMutablePointer, bufMutablePointer : UnsafeMutablePointer<UInt8>
+    private let dataPointer, bufPointer : UnsafePointer<UInt8>
+
+    public func threadFinished() -> Bool {
+        return background_network_thread_in!.isFinished && background_network_thread_out!.isFinished
     }
-    
+
+    // May be called in any thread
+    public func exitThreads() {
+        // Closing a stream makes it being unscheduled, this will force the run loop to exit
+        input_stream.close()
+        output_stream.close()
+    }
+
+    // Prepare threads and data buffers to handle a remote client
     public init(input_stream: InputStream, output_stream: OutputStream, from: NetServiceChargenDelegate) {
-        data = content.data(using: String.Encoding.utf8, allowLossyConversion: false)!
-        dataMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
-        data.copyBytes(to: dataMutablePointer, count: data.count)
+        self.input_stream = input_stream
+        self.output_stream = output_stream
+
+        // Prepare data buffers
+        dataMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: NetworkDefaults.buffer_size)
+        dataMutablePointer.initialize(repeating: 65, count: NetworkDefaults.buffer_size)
         dataPointer = UnsafePointer<UInt8>(dataMutablePointer)
-        bufMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: 10)
+        bufMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: NetworkDefaults.buffer_size)
         bufPointer = UnsafePointer<UInt8>(bufMutablePointer)
 
+        // Save callback
         self.from = from
 
+        // Initialize superclass
         super.init()
-        
+
+        // Create input and output threads
         input_stream.delegate = self
         output_stream.delegate = self
         background_network_thread_in = StreamNetworkThread(input_stream)
         background_network_thread_out = StreamNetworkThread(output_stream)
         
-        // Until now, input and output streams must only be accessed from their dedicated threads
+        // Beginning at this line, input and output streams must only be accessed from their dedicated threads
         background_network_thread_in!.start()
         background_network_thread_out!.start()
     }
 
-    deinit {
-        print("ChargenClient.deinit")
-    }
-    
+    // Manage callback I/O events
     public func stream(_ stream: Stream, handle event_code: Stream.Event) {
         switch event_code {
-        case .openCompleted:
-            print("openCompleted")
-            
         case .hasBytesAvailable:
-            //            print("hasBytesAvailable")
             let inputStream = stream as! InputStream
-            let ret = inputStream.read(bufMutablePointer, maxLength: 10)
+            let ret = inputStream.read(bufMutablePointer, maxLength: NetworkDefaults.buffer_size)
             if ret <= 0 { end(stream) }
-            //            print("read=->", ret)
-            
+
         case .hasSpaceAvailable:
-            //            print("hasSpaceAvailable")
             let outputStream = stream as! OutputStream
-            let ret = outputStream.write(dataPointer, maxLength: data.count)
+            let ret = outputStream.write(dataPointer, maxLength: NetworkDefaults.buffer_size)
             if ret < 0 { end(stream) }
-            //          print("write->", ret)
-            
+
         case .errorOccurred:
-            print("errorOccurred")
             end(stream)
             
         case .endEncountered:
-            print("endEncountered")
             end(stream)
             
         default:
-            print("default")
+            ()
         }
     }
     
     private func end(_ stream: Stream) {
-        print("END->CLOSE")
-        // Unschedule the stream, this will make the run loop to exit
+        // Closing the stream makes it being unscheduled, this will force the run loop to exit
         stream.close()
-        DispatchQueue.main.sync { self.from?.refClosed(self) }
+        // Inform the parent object that the stream has just been closed
+        DispatchQueue.main.async { self.from?.refClosed(self) }
     }
 }
 
+// Manage callbacks for the chargen service
 class NetServiceChargenDelegate : NSObject, NetServiceDelegate, RefClosed {
+    // Strong refs
     private var clients : [ChargenClient] = [ ]
 
+    // Initialize instance
+    public override init() {
+        // Initialize superclass
+        super.init()
+
+        // Add a background job that sweeps terminated connections
+        Timer.scheduledTimer(withTimeInterval: TimeInterval(1), repeats: true) {
+            _ in
+            var nothing_removed : Bool
+            repeat {
+                nothing_removed = true
+                for idx in self.clients.indices {
+                    if self.clients[idx].threadFinished() {
+                        self.clients.remove(at: idx)
+                        nothing_removed = false
+                        break
+                    }
+                }
+            } while nothing_removed == false
+        }
+    }
+
+    // If one client stream is closed, close the other to end communications with this client
     public func refClosed(_ client: ChargenClient) {
-        print("REMOVE")
-        client.cancelThreads()
+        client.exitThreads()
     }
-    
+
+    // Wait in the main thread until stream dedicated threads quit, in order to avoid discarding objects (strongly referenced by ChargenClient properties in 'clients' array) accessed by those threads. This will freeze the GUI until every clients are disconnected.
     deinit {
-        print("NetServiceChargenDelegate.deinit")
+        for client in clients { client.exitThreads() }
+        while true {
+            if clients.count == 0 { break }
+            sleep(1)
+        }
     }
-    
+
     public func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
         print("didNotPublish")
     }
@@ -179,29 +202,14 @@ class NetServiceChargenDelegate : NSObject, NetServiceDelegate, RefClosed {
         print("didUpdateTXTRecord")
     }
     
-    // Manage new connections
+    // Manage new connections from clients
     public func netService(_ sender: NetService, didAcceptConnectionWith inputStream: InputStream, outputStream: OutputStream) {
-        print("ACCEPT")
-        
-        // Sweep previous connections
-        var empty : Bool
-        repeat {
-            empty = true
-            for idx in clients.indices {
-                if clients[idx].background_network_thread_in!.isFinished && clients[idx].background_network_thread_out!.isFinished {
-                    clients.remove(at: idx)
-                    empty = false
-                    break
-                }
-            }
-        } while empty == false
-
         clients.append(ChargenClient(input_stream: inputStream, output_stream: outputStream, from: self))
     }
 }
 
 
-class MyNetServiceBrowserDelegate : NSObject, NetServiceBrowserDelegate {
+class NetServiceChargenBrowserDelegate : NSObject, NetServiceBrowserDelegate {
     deinit {
         print("MyNetServiceBrowserDelegate.deinit")
     }
@@ -235,12 +243,10 @@ class MyNetServiceBrowserDelegate : NSObject, NetServiceBrowserDelegate {
         print("netServiceBrowserDidStopSearch")
     }
 }
+
 class NetTools {
-    private static var net_service_chargen : NetService?
-    public static var net_service_chargen_delegate : NetServiceChargenDelegate?
-    
     private static var br : NetServiceBrowser?
-    public static var dl2 : MyNetServiceBrowserDelegate?
+    public static var dl2 : NetServiceChargenBrowserDelegate?
     
     public static var x = false
     
@@ -249,17 +255,12 @@ class NetTools {
             x = true
             
             // Create chargen service
-            net_service_chargen = NetService(domain: "local.", type: "_chargen._tcp.", name: "chargen", port: NetworkDefaults.chargen_port)
-            
-            // Add a strong ref to the delegate since NetService.delegate is declared unowned(unsafe)
-            net_service_chargen_delegate = NetServiceChargenDelegate()
-            net_service_chargen!.delegate = net_service_chargen_delegate
+            let net_service_chargen = NetService(domain: "local.", type: "_chargen._tcp.", name: "chargen", port: NetworkDefaults.chargen_port)
+            net_service_chargen.delegate = NetServiceChargenDelegate()
             
             // Start listening for chargen clients
-            net_service_chargen!.publish(options: .listenForConnections)
-            
-            
-            
+            net_service_chargen.publish(options: .listenForConnections)
+
             //            let browser = NetServiceBrowser()
             //            br = browser
             //            dl2 = MyNetServiceBrowserDelegate()
