0b0c3f0f8367bd0e0167c5eaf5faa91b3111a443
Alexandre Fenyo <nospam@fenyo.net>
2018-07-28 00:57:33 +0200

    update

--- iOS tools/SpeedTest/DetailViewController.swift
+++ iOS tools/SpeedTest/DetailViewController.swift
@@ -34,13 +34,17 @@ class DetailViewController: UIViewController {
 
         navigationItem.leftItemsSupplementBackButton = true
 
-        let scene = SKScene(size: CGSize(width: view.frame.size.width / 2, height: view.frame.size.height))
+        // CGSize(width: view.frame.size.width / 2, height: view.frame.size.height / 4)
+        let scene_size = ingress_chart.bounds.size
+        let full_size = ingress_chart.bounds.size
+
+        let scene = SKScene(size: scene_size)
         scene.backgroundColor = .brown
         ingress_chart.presentScene(scene)
 
-        chart_node = SKChartNode(ts: ts, full_size: CGSize(width: 410, height: 300), grid_size: CGSize(width: 20, height: 20), subgrid_size: CGSize(width: 5, height: 5), line_width: 1, left_width: 80, bottom_height: 50, vertical_unit: "Kbit/s", grid_vertical_cost: 10, date: Date(), grid_time_interval: 2, background: .gray, max_horizontal_font_size: 10, max_vertical_font_size: 20, spline: true, vertical_auto_layout: true, debug: false)
+        chart_node = SKChartNode(ts: ts, full_size: full_size, grid_size: CGSize(width: 20, height: 20), subgrid_size: CGSize(width: 5, height: 5), line_width: 1, left_width: 80, bottom_height: 50, vertical_unit: "Kbit/s", grid_vertical_cost: 10, date: Date(), grid_time_interval: 2, background: .gray, max_horizontal_font_size: 10, max_vertical_font_size: 20, spline: true, vertical_auto_layout: true, debug: false)
         scene.addChild(chart_node!)
-        chart_node!.position = CGPoint(x: 50, y: 100)
+        chart_node!.position = CGPoint(x: -20, y: -20)
         chart_node!.registerGestureRecognizers(view: view)
 
 //        ingress_chart.showsFPS = true
