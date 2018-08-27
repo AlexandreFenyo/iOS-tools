81e4d74cb609b5d35b47681d7c5d2c485186705f
Alexandre Fenyo <fenyo@mac-de-alexandre.local>
2018-08-27 21:48:32 +0200

    update

--- iOS tools/Tools/Networks/GenericNetTools.swift
+++ iOS tools/Tools/Networks/GenericNetTools.swift
@@ -189,30 +189,30 @@ class IPv6Address : IPAddress {
 }
 
 class GenericNetTools {
-    // Data must be a sockaddr, sockaddr_in or sockaddr_in6 structure
-    public static func getHostNameFromSockAddr(_ addr: Data) -> String {
-        return addr.withUnsafeBytes {
-            (saddr : UnsafePointer<sockaddr>) -> String in
-            var buffer = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-            getnameinfo(saddr, UInt32(NSData(data: addr).length), &buffer, UInt32(NI_MAXHOST), nil, 0, NI_NAMEREQD)
-            return String(cString: buffer)
-        }
-    }
-
-    // Data must be a sockaddr, sockaddr_in or sockaddr_in6 structure
-    public static func getAddrFromSockAddr(_ addr: Data) -> String {
-        return addr.withUnsafeBytes {
-            (saddr : UnsafePointer<sockaddr>) -> String in
-            var buffer = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-            getnameinfo(saddr, UInt32(NSData(data: addr).length), &buffer, UInt32(NI_MAXHOST), nil, 0, NI_NUMERICHOST)
-            return String(cString: buffer)
-        }
-    }
-
-    // Result may be compared to AF_INET, AF_INET6, etc.
-    public static func getAddrFamilyFromSockAddr(_ addr: Data) -> Int32 {
-        var saddr = sockaddr_storage()
-        NSData(data: addr).getBytes(&saddr, length: MemoryLayout<sockaddr>.size)
-        return Int32(saddr.ss_family)
-    }
+//    // Data must be a sockaddr, sockaddr_in or sockaddr_in6 structure
+//    public static func getHostNameFromSockAddr(_ addr: Data) -> String {
+//        return addr.withUnsafeBytes {
+//            (saddr : UnsafePointer<sockaddr>) -> String in
+//            var buffer = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+//            getnameinfo(saddr, UInt32(NSData(data: addr).length), &buffer, UInt32(NI_MAXHOST), nil, 0, NI_NAMEREQD)
+//            return String(cString: buffer)
+//        }
+//    }
+//
+//    // Data must be a sockaddr, sockaddr_in or sockaddr_in6 structure
+//    public static func getAddrFromSockAddr(_ addr: Data) -> String {
+//        return addr.withUnsafeBytes {
+//            (saddr : UnsafePointer<sockaddr>) -> String in
+//            var buffer = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+//            getnameinfo(saddr, UInt32(NSData(data: addr).length), &buffer, UInt32(NI_MAXHOST), nil, 0, NI_NUMERICHOST)
+//            return String(cString: buffer)
+//        }
+//    }
+//
+//    // Result may be compared to AF_INET, AF_INET6, etc.
+//    public static func getAddrFamilyFromSockAddr(_ addr: Data) -> Int32 {
+//        var saddr = sockaddr_storage()
+//        NSData(data: addr).getBytes(&saddr, length: MemoryLayout<sockaddr>.size)
+//        return Int32(saddr.ss_family)
+//    }
 }
