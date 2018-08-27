090ce10b317516da2120b3dfe72097f1fa396218
Alexandre Fenyo <fenyo@mac-de-alexandre.local>
2018-08-27 21:45:45 +0200

    update

--- iOS tools/SpeedTest/Networks/Browsers/NetworkBrowser.swift
+++ iOS tools/SpeedTest/Networks/Browsers/NetworkBrowser.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 class NetworkBrowser {
-    private let network : IPv4SAddress
-    private let netmask : IPv4SAddress
+    private let network : IPv4Address
+    private let netmask : IPv4Address
 
-    public init?(network: IPv4SAddress?, netmask: IPv4SAddress?) {
+    public init?(network: IPv4Address?, netmask: IPv4Address?) {
         if network == nil || netmask == nil { return nil }
         self.network = network!
         self.netmask = netmask!
