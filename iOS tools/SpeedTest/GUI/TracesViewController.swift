9bff5658fd4f3ab3341c2197825f0c2915d3061e
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2021-10-26 12:04:06 +0200

    affichage d'une view SwiftUI embedded dans UIKit

---
+++ iOS tools/SpeedTest/GUI/TracesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TracesViewController.swift
+//  iOS tools
+//
+//  Created by Alexandre Fenyo on 26/10/2021.
+//  Copyright © 2021 Alexandre Fenyo. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import SwiftUI
+
+class TracesViewController : UIViewController {
+    let contentView = UIHostingController(rootView: TracesSwiftUIView())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(contentView.view)
+        contentView.view.frame = view.frame
+        //        addChild(contentView)
+        //        contentView.didMove(toParent: self)
+    }
+}
