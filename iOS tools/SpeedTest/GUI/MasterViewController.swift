694511ceb294e68f66be05ff5e77c445c15f4808
Alexandre Fenyo <github@fenyo.net>
2022-09-14 22:13:34 +0200

    changement d'écran si click sur node

--- iOS tools/SpeedTest/GUI/MasterViewController.swift
+++ iOS tools/SpeedTest/GUI/MasterViewController.swift
@@ -635,6 +635,10 @@ class MasterViewController: UITableViewController, DeviceManager {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        detail_view_controller!.node = getNode(indexPath: indexPath)
         stopBrowsing(.OTHER_ACTION)
+        
+        // for iPhone (pas d'effet sur iPad), make the detail view controller visible
+        splitViewController?.showDetailViewController(detail_navigation_controller!, sender: nil)
+
     }
 
     // Local gateway and Internet rows can not be removed
