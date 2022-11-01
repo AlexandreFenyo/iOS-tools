0172485889ee470ccf4b344b02cc0f424400fefb
Alexandre Fenyo <github@fenyo.net>
2022-11-01 17:16:13 +0100

    traitement des images au format webp

--- iOS tools/SpeedTest/GUI/SwiftUI/ImagePicker.swift
+++ iOS tools/SpeedTest/GUI/SwiftUI/ImagePicker.swift
@@ -6,7 +6,7 @@ import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     @Binding var image: UIImage?
-
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
@@ -14,31 +14,40 @@ struct ImagePicker: UIViewControllerRepresentable {
         picker.delegate = context.coordinator
         return picker
     }
-
+    
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
     }
-
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
     class Coordinator: NSObject, PHPickerViewControllerDelegate {
         let parent: ImagePicker
-
+        
         init(_ parent: ImagePicker) {
             self.parent = parent
         }
-
+        
         func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
             picker.dismiss(animated: true)
-
+            
             guard let provider = results.first?.itemProvider else { return }
 
-            if provider.canLoadObject(ofClass: UIImage.self) {
-                provider.loadObject(ofClass: UIImage.self) { image, _ in
-                    Task {
-                        print("XXXX picker ici")
-                        self.parent.image = image as? UIImage
+            if provider.hasItemConformingToTypeIdentifier(UTType.webP.identifier) {
+                provider.loadDataRepresentation(forTypeIdentifier: UTType.webP.identifier) {data, err in
+                    if let data = data, let image = UIImage.init(data: data) {
+                        Task {
+                            self.parent.image = image
+                        }
+                    }
+                }
+            } else {
+                if provider.canLoadObject(ofClass: UIImage.self) {
+                    provider.loadObject(ofClass: UIImage.self) { image, _ in
+                        Task {
+                            self.parent.image = image as? UIImage
+                        }
                     }
                 }
             }
