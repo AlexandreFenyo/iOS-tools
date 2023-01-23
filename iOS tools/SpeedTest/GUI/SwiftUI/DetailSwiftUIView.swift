f478004032d46eb64bc0386099c80b35dbea8642
Alexandre Fenyo <github@fenyo.net>
2023-01-23 14:31:30 +0100

    traduction des popup d'action

--- iOS tools/SpeedTest/GUI/SwiftUI/DetailSwiftUIView.swift
+++ iOS tools/SpeedTest/GUI/SwiftUI/DetailSwiftUIView.swift
@@ -214,7 +214,8 @@ struct DetailSwiftUIView: View {
                         HStack(alignment: .top) {
                             Button {
                                 if model.address != nil {
-                                    master_view_controller.popUpHelp(.scan_TCP_ports, "Parallel TCP connections will be established to \(model.address_str ?? "") on TCP ports from 1 to 65535, to find open services. The new discovered services will be displayed on the bottom view. You can interrupt this task by pressing the STOP button.") {
+                                    // CONTINUER ICI
+                                    master_view_controller.popUpHelp(.scan_TCP_ports, NSLocalizedString("Parallel TCP connections will be established to ", comment: "Parallel TCP connections will be established to ") + (model.address_str ?? "") + NSLocalizedString(" on TCP ports from 1 to 65535, to find open services. The new discovered services will be displayed on the bottom view. You can interrupt this task by pressing the STOP button.", comment: " on TCP ports from 1 to 65535, to find open services. The new discovered services will be displayed on the bottom view. You can interrupt this task by pressing the STOP button.")) {
                                         master_view_controller.scanTCP(model.address!)
                                     }
                                 }
@@ -229,7 +230,7 @@ struct DetailSwiftUIView: View {
                             
                             Button {
                                 if model.address != nil {
-                                    master_view_controller.popUpHelp(.TCP_flood_discard, "A TCP connection to the Discard port (9/TCP) of \(model.address_str ?? "") will be established. Data will then be sent on this connection at the maximum throughput available, by this device to this target host, to evaluate the maximum speed that can be reached in the outgoing direction. You can interrupt this task by pressing the STOP button.") {
+                                    master_view_controller.popUpHelp(.TCP_flood_discard, NSLocalizedString("A TCP connection to the Discard port (9/TCP) of ", comment: "A TCP connection to the Discard port (9/TCP) of ") + (model.address_str ?? "") + NSLocalizedString(" will be established. Data will then be sent on this connection at the maximum throughput available, by this device to this target host, to evaluate the maximum speed that can be reached in the outgoing direction. You can interrupt this task by pressing the STOP button.", comment: " will be established. Data will then be sent on this connection at the maximum throughput available, by this device to this target host, to evaluate the maximum speed that can be reached in the outgoing direction. You can interrupt this task by pressing the STOP button.")) {
                                         master_view_controller.floodTCP(model.address!)
                                     }
                                 }
@@ -244,7 +245,7 @@ struct DetailSwiftUIView: View {
                             
                             Button {
                                 if model.address != nil {
-                                    master_view_controller.popUpHelp(.TCP_flood_chargen, "A TCP connection to the Chargen port (19/TCP) of \(model.address_str ?? "") will be established. Data will then be received on this connection at the maximum throughput available, by this device from this target host, to evaluate the maximum speed that can be reached in the incoming direction. You can interrupt this task by pressing the STOP button.") {
+                                    master_view_controller.popUpHelp(.TCP_flood_chargen, NSLocalizedString("A TCP connection to the Chargen port (19/TCP) of ", comment: "A TCP connection to the Chargen port (19/TCP) of ") + (model.address_str ?? "") + NSLocalizedString(" will be established. Data will then be received on this connection at the maximum throughput available, by this device from this target host, to evaluate the maximum speed that can be reached in the incoming direction. You can interrupt this task by pressing the STOP button.", comment: " will be established. Data will then be received on this connection at the maximum throughput available, by this device from this target host, to evaluate the maximum speed that can be reached in the incoming direction. You can interrupt this task by pressing the STOP button.")) {
                                         master_view_controller.chargenTCP(model.address!)
                                     }
                                 }
@@ -272,7 +273,7 @@ struct DetailSwiftUIView: View {
                             
                             Button {
                                 if model.address != nil {
-                                    master_view_controller.popUpHelp(.ICMP_ping, "ICMP packets of type ECHO_REQUEST will be sent to \(model.address_str ?? "") at a rate of one packet per second. The target should reply with an ICMP packet of type ECHO_REPLY. The round trip type is computed and displayed on the chart. You can interrupt this task by pressing the STOP button.") {
+                                    master_view_controller.popUpHelp(.ICMP_ping, NSLocalizedString("ICMP packets of type ECHO_REQUEST will be sent to ", comment: "ICMP packets of type ECHO_REQUEST will be sent to ") + (model.address_str ?? "") + NSLocalizedString(" at a rate of one packet per second. The target should reply with an ICMP packet of type ECHO_REPLY. The round trip type is computed and displayed on the chart. You can interrupt this task by pressing the STOP button.", comment: " at a rate of one packet per second. The target should reply with an ICMP packet of type ECHO_REPLY. The round trip type is computed and displayed on the chart. You can interrupt this task by pressing the STOP button.")) {
                                         master_view_controller.loopICMP(model.address!)
                                     }
                                 }
