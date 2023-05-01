6270c3e511d6cbdefed3153bea6eadd03cd94bd9
Alexandre Fenyo <github@fenyo.net>
2023-05-01 10:24:24 +0200

    test

--- iOS tools/Tools/Interman/Interman3DModel.swift
+++ iOS tools/Tools/Interman/Interman3DModel.swift
@@ -115,8 +115,6 @@ public class Interman3DModel : ObservableObject {
         b3d_nodes.append(b3d_node)
 
         print("addComponent(node) done")
-
-        
     }
     
     internal func addComponent() {
