33672c3f9cdefd2d98cb4ddbac2b41fe8509b64f
Alexandre Fenyo <github@fenyo.net>
2024-02-18 23:26:12 +0100

    create a dynamic ports to boujour services table

---
+++ iOS tools/Tools/Networks/Ports2BonjourServices.swift
@@ -0,0 +1,9 @@
+//
+//  Ports2BonjourServices.swift
+//  iOS tools
+//
+//  Created by Alexandre Fenyo on 18/02/2024.
+//  Copyright © 2024 Alexandre Fenyo. All rights reserved.
+//
+
+import Foundation
