e26e6b86b828ebb5f04d0b874d3d8cf1f8336098
Alexandre Fenyo <github@fenyo.net>
2024-04-21 23:08:32 +0200

    update

--- countlines.sh
+++ countlines.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-wc -l ./iOS\ tools/**/*.swift ./iOSToolsMacros/**/*.swift iOS\ tools/**/*.[ch]
+wc -l ./iOS\ tools/**/*.swift ./iOSToolsMacros/**/*.swift iOS\ tools/**/*.[ch] ../SwiftAdvancedAtomicsCPackage/Sources/SwiftAdvancedAtomicsCPackage/**/*.[ch](.) ../SwiftAdvancedAtomicsSwiftPackage/Sources/SwiftAdvancedAtomicsSwiftPackage/*swift(.) ../iOSToolsMacros/Sources/**/*.swift(.)
 
